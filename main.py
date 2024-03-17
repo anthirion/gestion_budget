7a825104a9c52560862ac929ce359c8ef0c05b71
anthirion <antoth.web@outlook.com>
2024-03-17 15:06:45 +0100

    création d'un argument qui indique s'il est nécessaire de recréer la source de vérité à partir des csv fournis pas LCL

--- main.py
+++ main.py
@@ -7,25 +7,44 @@ import argparse
 
 
 if __name__ == "__main__":
-    raw_csv_directory = "/home/thiran/projets_persos/gestion_budget/csv_files/raw_csv_files/"
     """
-    On rassemble et nettoie toutes les transactions dans un fichier csv unique
-    qui sera notre source de vérité
+    On récupére les arguments que l'utilisateur fournis via la ligne de commande
+    pour les traiter plus tard
     """
-    # on retire les transactions qui apparaissent en double
-    unique_lines = create_unique_csv.extract_unique_lines(raw_csv_directory)
-    # on trie ensuite les transactions par date croissante
-    unique_lines = list(unique_lines)
-    unique_lines.sort(key=create_unique_csv.sort_by_transaction_date,
-                      reverse=False)
-    # on écrit les transactions obtenues dans la source de vérité
-    clean_csv_filename = "/home/thiran/projets_persos/gestion_budget/csv_files/clean_csv_files/source_of_truth.csv"
-    with open(clean_csv_filename, "w", encoding="utf-8") as file:
-        # la première ligne spécifie les noms des colonnes
-        column_names = "Date,Montant,Type,Description\n"
-        file.write(column_names)
-        for line in unique_lines:
-            file.write(line)
+    parser = argparse.ArgumentParser()
+    # on crée l'argument qui indique s'il est nécessaire de créer la source de vérité ou non
+    parser.add_argument(
+        "--creer_source_de_verite", help="indique s'il est nécessaire de créer la source de vérité",
+        type=bool, default=False)
+    # on crée les arguments mois et année fournis par l'utilisateur à travers la ligne de commande
+    parser.add_argument(
+        "--mois", help="le nombre de mois à analyser", type=int, default=1)
+    parser.add_argument(
+        "--annee", help="le nombre d'années à analyser", type=int, default=0)
+    # on récupère tous les arguments fournis par l'utilisateur
+    args = parser.parse_args()
+    """
+    On rassemble et nettoie toutes les transactions fournies par LCL 
+    dans un fichier csv unique qui sera notre source de vérité
+    uniquement si nécessaire (préciser par l'utilisateur)
+    """
+    if args.creer_source_de_verite is True:
+        raw_csv_directory = "/home/thiran/projets_persos/gestion_budget/csv_files/raw_csv_files/"
+        # on retire les transactions qui apparaissent en double
+        unique_lines = create_unique_csv.extract_unique_lines(
+            raw_csv_directory)
+        # on trie ensuite les transactions par date croissante
+        unique_lines = list(unique_lines)
+        unique_lines.sort(key=create_unique_csv.sort_by_transaction_date,
+                          reverse=False)
+        # on écrit les transactions obtenues dans la source de vérité
+        clean_csv_filename = "/home/thiran/projets_persos/gestion_budget/csv_files/clean_csv_files/source_of_truth.csv"
+        with open(clean_csv_filename, "w", encoding="utf-8") as file:
+            # la première ligne spécifie les noms des colonnes
+            column_names = "Date,Montant,Type,Description\n"
+            file.write(column_names)
+            for line in unique_lines:
+                file.write(line)
     """
     On sélectionne les transactions du dernier mois (mois de mars 2023)
     """
@@ -35,13 +54,6 @@ if __name__ == "__main__":
     transactions = transactions.split(("\n"))
     # on retire la première ligne qui correspond aux colonnes
     transactions = transactions[1:]
-    # on récupère les arguments mois et année fournis par l'utilisateur à travers la ligne de commande
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--mois", help="le nombre de mois à analyser", type=int, default=1)
-    parser.add_argument(
-        "--annee", help="le nombre d'années à analyser", type=int, default=0)
-    args = parser.parse_args()
     transactions_dernier_mois = select_transactions.select_transactions(transactions,
                                                                         n_month=args.mois,
                                                                         n_year=args.annee)
