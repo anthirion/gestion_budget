8ea1b083ba5e7fea6df92ac504da4d672f930b33
anthirion <antoth.web@outlook.com>
2024-03-17 11:22:58 +0100

    adaptation à une liste de transactions plutôt qu'à un fichier d'entrée

--- camembert.py
+++ camembert.py
@@ -1,6 +1,6 @@
 """
 L'objectif de ce module est de construire un camembert des dépenses du mois en fonction des catégories
-de dépenses
+de dépenses. Il prend en entrée une liste des transactions à traiter.
 """
 import sys
 import matplotlib.pyplot as plt
@@ -8,34 +8,33 @@ from collections import defaultdict
 import copy
 
 
-def calculer_depenses_par_categories(filename, condenser=False):
+def calculer_depenses_par_categories(transactions, condenser=False):
     """
+    @parameter transactions : liste des transactions à traiter
     retourne un dictionnaire dont la clé correspond à une catégorie de dépense
     et la valeur correspondante est la somme des montants dépensés dans cette catégorie
     si condenser vaut False, le dictionnaire contient l'ensemble des dépenses
     sinon le dictionnaire regroupera certaines dépenses dans une catégorie "Autre" 
     """
     depenses = defaultdict(int)
-    with open(filename, "r", encoding="utf-8") as csv_file:
-        # ignorer la première ligne
-        next(csv_file)
-        for line in csv_file:
-            try:
-                _, montant, type_transaction, description = line.split(",")
-                # on ne prend en compte que les transactions par carte
-                if str(type_transaction) == "Carte":
-                    depenses[description] += -float(montant)
-            except ValueError as e:
-                # dans le cas où le sequence unpacking ne marche pas
-                print(type(e), e)
-                # line[:-1] pour retirer le saut de ligne lors du print
-                print("Ligne qui pose problème: ", line[:-1])
-                print("Si c'est la dernière ligne, c'est ok ;) \n")
+    for transaction in transactions:
+        try:
+            _, montant, type_transaction, description = transaction.split(",")
+            # on ne prend en compte que les transactions par carte
+            if str(type_transaction) == "Carte":
+                depenses[description] += -float(montant)
+        except ValueError as e:
+            # dans le cas où le sequence unpacking ne marche pas
+            print(type(e), e)
+            # transaction[:-1] pour retirer le saut de ligne lors du print
+            print("Ligne qui pose problème: ", transaction[:-1])
+            print("Si c'est la dernière ligne, c'est ok ;) \n")
 
     """ 
     Pour éviter que les valeurs du dictionnaire soient illisibles sur un 
     camembert, on classe toutes les catégories dont le montant est inférieur
     à 2% de la somme des dépenses dans une catégorie "Autre"
+    uniquement si condenser est True
     """
     if condenser is True:
         somme_depenses = sum(montant for montant in depenses.values())
@@ -56,11 +55,11 @@ def calculer_depenses_par_categories(filename, condenser=False):
     return (depenses if condenser is False else depenses_condensees)
 
 
-def display_pie_chart(csv_filename):
+def display_pie_chart(transactions):
     """
-    Affiche un camembert des dépenses indiquées dans le fichier passé en paramètre
+    Affiche un camembert des dépenses de la liste transactions
     """
-    depenses = calculer_depenses_par_categories(csv_filename, condenser=True)
+    depenses = calculer_depenses_par_categories(transactions, condenser=True)
     # afficher le camembert des dépenses avec les dépenses arrondies au centime près
     somme_depenses = sum(depenses.values())
     # plt.bar(depenses.keys(), depenses.values())
@@ -71,15 +70,3 @@ def display_pie_chart(csv_filename):
             )
     plt.title(f"Depenses totales du mois passé: {round(somme_depenses, 2)}€")
     plt.show()
-
-
-if __name__ == "__main__":
-    try:
-        csv_filename = "/home/thiran/projets_persos/gestion_budget/csv_files/clean_csv_files/" + \
-            sys.argv[1]
-        display_pie_chart(csv_filename)
-    except IndexError as e:
-        print("Nombre d'arguments fournis incorrect !")
-        print(
-            "Vous devez fournir le nom du fichier csv qui servira à dessiner le camembert")
-        print("L'erreur est la suivante :", e)
