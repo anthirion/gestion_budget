97709afddcb2b9bb03478df63d547e0d41fe3213
anthirion <antoth.web@outlook.com>
2024-03-17 11:22:19 +0100

    selection des transactions par mois fonctionne; essayer la selection par annee lorsqu'il y aura suffisamment de transactions

--- select_transactions.py
+++ select_transactions.py
@@ -46,21 +46,23 @@ def get_last_month_year(transactions):
     """
     Retourne le dernier mois et la dernière année des transactions fournies
     """
-    try:
+    if is_a_transaction(transactions[0]):
         _, month, year = transactions[0].split(",")[0].split("/")
         for transaction in transactions[1:]:
-            _, current_month, current_year = transaction.split(",")[
-                0].split("/")
-            if (current_month != month):
-                month = current_month
-            if (current_year != year):
-                year = current_year
-        return (month, year)
-    except ValueError as ve:
-        print("Erreur: le nombre de paramètres unpacked est incorrect\n", ve)
+            if is_a_transaction(transaction):
+                _, current_month, current_year = transaction.split(",")[
+                    0].split("/")
+                if (current_month != month):
+                    month = current_month
+                if (current_year != year):
+                    year = current_year
+        return (int(month), int(year))
+    else:
+        raise ValueError(
+            f"La première ligne n'est pas une transaction: {transactions[0]}")
 
 
-def select_transactions(transactions, month=1, year=0):
+def select_transactions(transactions, n_month=1, n_year=0):
     """
     @parameter day: sélectionner la liste des transactions réaliséees les n derniers jours
     @parameter month: sélectionner la liste des transactions réaliséees les n derniers mois
@@ -69,16 +71,26 @@ def select_transactions(transactions, month=1, year=0):
     """
     selected_transactions = []
     last_month, last_year = get_last_month_year(transactions)
-    # on définit les premiers jour, mois et année à sélectionner
-    first_month = (last_month - month) % 12
-    first_year = last_year - year
+    # on définit les premiers jour, mois et année où commencer la sélection
+    n_month += n_year * 12 + 1
+    first_month = last_month - n_month
+    if first_month < 0:
+        first_year = last_year + first_month//12
+        first_month = first_month % 12 + 1
     for transaction in transactions:
         if is_a_transaction(transaction):
             _, current_month, current_year = transaction.split(",")[
                 0].split("/")
-            if (first_year <= current_year <= last_year and
-                    first_month <= current_month <= last_month):
+            # pour selectionner la transaction, on distingue trois cas:
+            # si l'annee courante est égale à la premiere annee, on laisse passer
+            # les transactions correspondantes aux mois >= first_month
+            # si l'annee courante est égale à la dernière annee, on laisse passer
+            # les transactions correspondantes aux mois <= last_month
+            # sinon, on laisse passer les transactions correspondantes à tous les mois
+            if (int(current_year) == first_year and int(current_month) >= first_month):
                 selected_transactions.append(transaction)
-        else:
-            print("not a transaction:", transaction)
+            elif (int(current_year) == last_year and int(current_month) <= last_month):
+                selected_transactions.append(transaction)
+            elif (first_year < int(current_year) < last_year):
+                selected_transactions.append(transaction)
     return selected_transactions
