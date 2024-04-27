2b6513b949ea7572065e92a0a5bc0da03e267abb
Antoine Thirion <antoth.web@outlook.fr>
2024-04-27 23:27:37 +0200

    Factorisation des widgets de paramètres et de sommes dans des fichiers séparés

--- gestion_budget_app/global_variables.py
+++
@@ -1,7 +0,0 @@
-pourcentage_cat_autres = 0.02
-pourcentage_affichage_label_pie_chart = 0.1
-source_of_truth = str()
-
-save_file = "/home/thiran/projets_persos/gestion_budget/src/gestion_budget_app/save.txt"
-source_of_truth_notfound_msg = "La source de vérité n'a pas été trouvée, veuillez l'indiquer pour pouvoir lancer les calculs"
-no_transaction_found_msg = "Aucune transaction sélectionnée ! Vérifiez que des transactions existent pour la période sélectionnée."
