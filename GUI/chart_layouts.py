3a0d857dc5c07e0944e21573ae7ae93ffa6ad997
Antoine Thirion <antoth.web@outlook.fr>
2024-04-30 22:01:38 +0200

    [refactor] refactoring des layouts d'affichage des sommes de dépenses mensuelles

---
+++ GUI/chart_layouts.py
@@ -0,0 +1,14 @@
+from PySide6.QtWidgets import (
+    QVBoxLayout, QCheckBox,
+)
+from PySide6.QtGui import QPainter
+from PySide6 import QtCharts
+
+
+class PieChartLayout:
+    """
+    Cette classe définit le layout correspondant aux camemberts
+    """
+
+    def __init__(self):
+        super().__init__()
--- GUI/sums_layout.py
+++ GUI/sums_layout.py
@@ -4,36 +4,53 @@ from PySide6.QtWidgets import (
 from PySide6.QtCore import Qt
 
 
+class SumLayout(QLayout):
+    """
+    Cette classe définit un layout affichant la somme de dépenses sur le mois sélectionné
+    Le type de la somme (par carte ou virement) est défini par le titre passé au constructeur 
+    """
+
+    def __init__(self, layout_title):
+        super().__init__()
+        self.title = layout_title
+
+        # définition du titre et label pour les dépenses
+        self.sum_layout = QVBoxLayout()
+        title = QLabel(self.title)
+        title.setAlignment(Qt.AlignCenter)
+        self.expenses_label = QLabel()
+        self.expenses_label.setAlignment(Qt.AlignCenter)
+
+        # ajout des widgets précédents au layout
+        self.sum_layout.addWidget(title)
+        self.sum_layout.addWidget(self.expenses_label)
+
+
 class SumsLayout(QLayout):
     def __init__(self):
         """"
-        Afficher la somme des dépenses par carte et par virement
-        sur le mois sélectionné
+        Cette classe définit deux layouts affichant des sommes de dépenses sur le mois sélectionné:
+            - la somme des dépenses par carte
+            - la somme des dépenses par virement
+
         """
         super().__init__()
 
         self.sums_layout = QHBoxLayout()
-        card_sum_layout = QVBoxLayout()
-        # définition du titre et label pour les dépenses par carte
-        card_title = QLabel(
-            "Somme des dépenses par carte sur le mois sélectionné:")
-        card_title.setAlignment(Qt.AlignCenter)
-        self.sum_card_expenses_label = QLabel()
-        self.sum_card_expenses_label.setAlignment(Qt.AlignCenter)
-        card_sum_layout.addWidget(card_title)
-        card_sum_layout.addWidget(self.sum_card_expenses_label)
 
-        bank_transfer_sum_layout = QVBoxLayout()
+        # ajout d'un widget affichant la somme des dépenses par carte
+        card_title = "Somme des dépenses par carte sur le mois sélectionné:"
+        card_sum = SumLayout(card_title)
+        self.card_expenses_label = card_sum.expenses_label
+        card_sum_layout = card_sum.sum_layout
+        # ajout d'un widget affichant la somme des dépenses par virement
+
         # définition du titre et label pour les dépenses par virement
-        bank_transfer_title = QLabel(
-            "Somme des dépenses par virement sur le mois sélectionné:")
-        bank_transfer_title.setAlignment(Qt.AlignCenter)
-        self.sum_bank_transfer_expenses_label = QLabel()
-        self.sum_bank_transfer_expenses_label.setAlignment(Qt.AlignCenter)
-        bank_transfer_sum_layout.addWidget(bank_transfer_title)
-        bank_transfer_sum_layout.addWidget(
-            self.sum_bank_transfer_expenses_label)
+        bank_transfer_title = "Somme des dépenses par virement sur le mois sélectionné:"
+        bank_transfer_sum = SumLayout(bank_transfer_title)
+        self.bank_transfer_expenses_label = bank_transfer_sum.expenses_label
+        bank_transfer_sum_layout = bank_transfer_sum.sum_layout
 
-        # affichage des widgets précédemment définis
+        # ajout des widgets de sommes au layout
         self.sums_layout.addLayout(card_sum_layout)
         self.sums_layout.addLayout(bank_transfer_sum_layout)
