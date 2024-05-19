5ed387b06c66773bbd0b7a4f1bb8eca000f85968
Antoine Thirion <antoth.web@outlook.fr>
2024-05-19 23:27:30 +0200

    Slots de connection entre les widgets et leur vue respective terminés

--- GUI/main_window.py
+++ GUI/main_window.py
@@ -3,7 +3,8 @@ from GUI.several_months_widget import SeveralMonthsWidget
 from GUI.menu_bar import MenuBar
 
 from PySide6.QtWidgets import (
-    QMainWindow, QLabel, QHBoxLayout, QWidget
+    QMainWindow, QHBoxLayout, QWidget, QLabel,
+    QStackedWidget
 )
 from PySide6.QtCore import Qt
 from GUI.side_menu import SideMenu
@@ -25,8 +26,43 @@ class MainWindow(QMainWindow):
             # barre de menus
             MenuBar(parent=self)
 
-            main_widget = QWidget(self)
-            page_layout = QHBoxLayout(main_widget)
+            # définition du widget et du layout de l'app
+            app_widget = QWidget(self)
+            app_layout = QHBoxLayout(app_widget)
+
+            """
+            Définition du widget principal (à côté du menu latéral)
+            Ce widget superpose plusieurs widgets:
+                - widget d'accueil (home_widget) qui s'affiche au démarrage
+                    de l'app
+                - les widgets correspondant aux sous-menus
+            """
+            # widget principal
+            self.main_widget = QStackedWidget(app_widget)
+
+            # widget d'accueil
+            home_widget = QLabel("Bienvenue sur mon app")
+            # widget du sous-menu des dépenses
+            expenses_widget = QLabel("Bienvenue sur la vue des dépenses")
+            # widget du sous-menu des revenus
+            revenus_widget = QLabel("Bienvenue sur la vue des revenus")
+            # widget du sous-menu patrimoine
+            assets_widget = QLabel("Bienvenue sur la vue du patrimoine")
+            # widget du sous-menu des transactions
+            transactions_widget = \
+                QLabel("Bienvenue sur la vue des transactions")
+
+            # centrer les widgets
+            for widget in [home_widget, expenses_widget, revenus_widget,
+                           assets_widget, transactions_widget]:
+                widget.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+            # ATTENTION: l'ordre est important !!!!
+            self.main_widget.addWidget(home_widget)
+            self.main_widget.addWidget(expenses_widget)
+            self.main_widget.addWidget(revenus_widget)
+            self.main_widget.addWidget(assets_widget)
+            self.main_widget.addWidget(transactions_widget)
 
             """
             Création d'un menu latéral avec plusieurs sous-menus:
@@ -36,7 +72,10 @@ class MainWindow(QMainWindow):
                 - Transactions
             """
             side_menu_widget = SideMenu(self)
-            page_layout.addWidget(side_menu_widget)
+
+            # disposition des widgets précédemment définis
+            app_layout.addWidget(side_menu_widget)
+            app_layout.addWidget(self.main_widget)
 
             """
             Création de plusieurs tabs:
@@ -50,11 +89,7 @@ class MainWindow(QMainWindow):
             #             "Statistiques sur plusieurs mois")
             # self.setCentralWidget(tabs)
 
-            label = QLabel("Bienvenue sur ma super app")
-            label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-            page_layout.addWidget(label)
-
-            self.setCentralWidget(main_widget)
+            self.setCentralWidget(app_widget)
 
         except Exception as e:
             # QMessageBox.critical(self, "Critical error", e)
--- GUI/side_menu.py
+++ GUI/side_menu.py
@@ -1,9 +1,22 @@
 from PySide6.QtWidgets import (
-    QWidget, QVBoxLayout, QPushButton,
+    QWidget, QVBoxLayout, QPushButton, QLabel
 )
 from PySide6.QtGui import QIcon
 import GUI.resources
 from PySide6.QtCore import Qt, Slot
+
+###############################################################################
+# On associe un index à chaque sous-menu
+###############################################################################
+
+expenses_widget_index = 1
+revenus_widget_index = 2
+assets_widget_index = 3
+transactions_widget_index = 4
+
+###############################################################################
+# Classe principale du module
+###############################################################################
 
 
 class SideMenu(QWidget):
@@ -24,30 +37,40 @@ class SideMenu(QWidget):
 
     def __init__(self, parent_widget):
         super().__init__(parent_widget)
+        # ce widget sert à afficher la bonne vue en fonction du sous-menu
+        # sélectionné depuis menu latéral
+        self.parent_main_widget = parent_widget.main_widget
+
         side_menu_layout = QVBoxLayout(self)
 
-        # définition des sous-menus
-        # définir chaque bouton "auto-exclusive" pour ne pas que 2 sous-menus
-        # soient sélectionnés en même temps
+        """
+        Définition des sous-menus
+        """
         expenses_sub_menu = QPushButton(icon=QIcon(":/icons/expenses.svg"),
                                         text="\t Dépenses")
-        expenses_sub_menu.setAutoExclusive(True)
-        expenses_sub_menu.setCheckable(True)
+        expenses_sub_menu.clicked.connect(self.expenses_selected)
 
         revenus_sub_menu = QPushButton(icon=QIcon(":/icons/money.svg"),
                                        text="\t Revenus")
-        revenus_sub_menu.setAutoExclusive(True)
-        revenus_sub_menu.setCheckable(True)
+        revenus_sub_menu.clicked.connect(self.revenus_selected)
 
         assets_sub_menu = QPushButton(icon=QIcon(":/icons/stocks.svg"),
                                       text="\t Patrimoine")
-        assets_sub_menu.setAutoExclusive(True)
-        assets_sub_menu.setCheckable(True)
+        assets_sub_menu.clicked.connect(self.assets_selected)
 
         transactions_sub_menu = QPushButton(icon=QIcon(":/icons/list.svg"),
                                             text="\t Transactions")
-        transactions_sub_menu.setAutoExclusive(True)
-        transactions_sub_menu.setCheckable(True)
+        transactions_sub_menu.clicked.connect(self.transactions_selected)
+
+        # définir chaque bouton "auto-exclusive" pour ne pas que 2 sous-menus
+        # soient sélectionnés en même temps
+        # et le rendre "checkable"
+        sub_menus = [expenses_sub_menu, revenus_sub_menu,
+                     assets_sub_menu, transactions_sub_menu]
+
+        for sub_menu in sub_menus:
+            sub_menu.setAutoExclusive(True)
+            sub_menu.setCheckable(True)
 
         side_menu_layout.addWidget(expenses_sub_menu)
         side_menu_layout.addWidget(revenus_sub_menu)
@@ -59,3 +82,38 @@ class SideMenu(QWidget):
         # fixer une taille maximale en largeur pour ne pas que les sous-menus
         # paraissent trop étirés horizontalement
         self.setMaximumWidth(300)
+
+    """
+    Buttons Slots
+    """
+    @Slot()
+    def expenses_selected(self):
+        """
+        Lorsque le sous-menu dépenses est sélectionné, afficher la vue des
+        dépenses
+        """
+        self.parent_main_widget.setCurrentIndex(expenses_widget_index)
+
+    @Slot()
+    def revenus_selected(self):
+        """
+        Lorsque le sous-menu revenus est sélectionné, afficher la vue des
+        revenus
+        """
+        self.parent_main_widget.setCurrentIndex(revenus_widget_index)
+
+    @Slot()
+    def assets_selected(self):
+        """
+        Lorsque le sous-menu patrimoine est sélectionné, afficher la vue du
+        patrimoine
+        """
+        self.parent_main_widget.setCurrentIndex(assets_widget_index)
+
+    @Slot()
+    def transactions_selected(self):
+        """
+        Lorsque le sous-menu transactions est sélectionné, afficher la vue des
+        transactions
+        """
+        self.parent_main_widget.setCurrentIndex(transactions_widget_index)
